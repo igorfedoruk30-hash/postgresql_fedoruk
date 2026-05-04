2609fed7c287a131f47cb31c7daca32b7dd2afc8
igorfedoruk30-hash <igorfedoruk30@gmail.com>
2026-05-04 20:59:06 +0300

    Update user_db.sql

--- user_db.sql
+++ user_db.sql
@@ -5,3 +5,13 @@ first_name varchar(64) not null,
 last_name varchar(64) not null,
 middle_name varchar(64) not null
 );
+
+CREATE TABLE address (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES schema_14(id),
+    zip_code VARCHAR(10),
+    city VARCHAR(100),
+    street VARCHAR(100),
+    building VARCHAR(10),
+    flat VARCHAR(10)
+);
